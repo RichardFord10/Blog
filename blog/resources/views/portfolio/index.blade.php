03f20b238aeb49fd1df42686a16d59928bf17993
richard ford <rf033@bucknell.edu>
2024-02-02 17:01:08 -0500

    Add first name and last name fields to Portfolio model and views

--- blog/resources/views/portfolio/index.blade.php
+++ blog/resources/views/portfolio/index.blade.php
@@ -17,8 +17,11 @@
             <div class="mt-4 px-6 lg:px-20">
                 <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-4">👋 Hello,</h2>
                 <h3 class="text-xl md:text-2xl font-bold text-gray-800 dark:text-white mb-4">I'm 
-                    @isset(Auth::user()->name)
-                        {{Auth::user()->name}}
+                    @isset($portfolio->first_name)
+                        {{ $portfolio->first_name }}
+                    @endisset
+                    @isset($portfolio->last_name)
+                        {{ $portfolio->last_name }}
                     @endisset
                 </h3>
                 <p class="text-gray-600 dark:text-gray-300 text-sm md:text-base leading-relaxed" data-aos="fade-up"
