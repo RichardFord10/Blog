a32427e3bc6a0f495eacd3e323c1da441d371a62
Richard Ford <36119878+RichardFord10@users.noreply.github.com>
2024-02-03 00:33:11 -0500

    update name stuff

--- blog/resources/views/portfolio/partials/about.blade.php
+++ blog/resources/views/portfolio/partials/about.blade.php
@@ -1,7 +1,14 @@
 <section>
     <div class="mt-4 px-6 lg:px-20">
         <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-4">👋 Hello,</h2>
-        <h3 class="text-xl md:text-2xl font-bold text-gray-800 dark:text-white mb-4">I'm Richard Ford</h3>
+        <h3 class="text-xl md:text-2xl font-bold text-gray-800 dark:text-white mb-4">I'm 
+            @isset($portfolio->first_name)
+                {{$portfolio->first_name}}
+            @endisset
+            @isset($portfolio->last_name)
+                {{$portfolio->last_name}}
+            @endisset
+        </h3>
         <p class="text-gray-600 dark:text-gray-300 text-sm md:text-base leading-relaxed" data-aos="fade-up"
             data-aos-anchor-placement="top-center">
             @isset($portfolio->about)
