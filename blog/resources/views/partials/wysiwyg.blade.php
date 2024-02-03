4a835a7b854192e668d12fc52ae1b1042c7f4ae2
Richard Ford <36119878+RichardFord10@users.noreply.github.com>
2024-02-03 14:16:47 -0500

    goofed a value

--- blog/resources/views/partials/wysiwyg.blade.php
+++ blog/resources/views/partials/wysiwyg.blade.php
@@ -11,6 +11,6 @@
             class="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2">{{ $label }}</label>
         <textarea name="{{ $field }}" id="{{ $field }}" placeholder="{{ $label }}"
             class="shadow appearance-none border rounded w-full h-48 py-2 px-3 text-gray-700 dark:text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
-            >{{ $entity->value ?? '' }}</textarea>
+            >{{ $value }}</textarea>
     </div>
 </section>
