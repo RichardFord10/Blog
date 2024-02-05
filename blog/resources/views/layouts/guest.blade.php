f85d6f5c2908148804bf2fe0940c7b989d18c403
Richard Ford <36119878+RichardFord10@users.noreply.github.com>
2024-02-04 19:35:09 -0500

    update logo

--- blog/resources/views/layouts/guest.blade.php
+++ blog/resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Richard Ford') }}</title>
+        <link rel="icon" type="image/svg+xml" href="/logo/code-tag.svg">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
