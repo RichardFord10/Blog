9fbb82375d0347c06e4a2a642295bad1df8f3f06
Richard Ford <richardford10@gmail.com>
2024-02-29 12:57:50 -0500

    fix name

--- blog/resources/views/components/name.blade.php
+++ blog/resources/views/components/name.blade.php
@@ -5,7 +5,7 @@
             Name:</label>
         <input type="text" id="first_name" name="first_name" placeholder="First Name"
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
-            value="{{ isset($entity->first_name) ?? '' }}" required>
+            value="{{ isset($entity->first_name) ? $entity->first_name : '' }}" required>
     </div>
 
     <!-- Last Name Input -->
@@ -13,6 +13,6 @@
         <label for="last_name" class="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2">Last Name:</label>
         <input type="text" id="last_name" name="last_name" placeholder="Last Name"
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
-            value="{{ isset($entity->last_name) ?? '' }}" required>
+            value="{{ isset($entity->last_name) ? $entity->last_name : '' }}" required>
     </div>
 </div>
