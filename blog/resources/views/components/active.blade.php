6d5a0419fedfd8d703ed24c75be9301312b03cc1
Richard Ford <richardford10@gmail.com>
2024-02-29 12:56:16 -0500

    fix portoflio error

--- blog/resources/views/components/active.blade.php
+++ blog/resources/views/components/active.blade.php
@@ -3,9 +3,11 @@
         <label for="active" class="inline-flex items-center">
             <!-- Hidden input to ensure a value is sent when checkbox is unchecked -->
             <input type="hidden" name="active" value="0" />
-            <input type="checkbox" id="active" name="active" value="1"
-                class="shadow border rounded text-blue-500 focus:outline-none focus:shadow-outline"
-                {{ $entity->active ? 'checked' : '' }} />
+            <input type="checkbox" id="active" name="active" value="1" class="shadow border rounded text-blue-500 focus:outline-none focus:shadow-outline" 
+            @if($entity && $entity->active)
+                checked
+            @endif
+
             <span class="ml-2 text-gray-700 dark:text-gray-200 text-sm font-bold">Active</span>
         </label>
     </div>
