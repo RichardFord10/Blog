d1155bf70d5ff2d863e1a6d8bd6466a90bb3aac4
Richard Ford <richardford10@gmail.com>
2024-04-13 15:37:47 -0400

    aos

--- blog/resources/views/components/portfolio/layout.blade.php
+++ blog/resources/views/components/portfolio/layout.blade.php
@@ -3,17 +3,23 @@
         <!-- Profile Section -->
         <div class="text-center max-w-4xl mx-auto p-4">
             <!-- Photo -->
-            <x-portfolio.photo :entity="$entity->portfolio->data"/>
+            <x-portfolio.photo :entity="$entity->portfolio->data" />
             <!-- About Me -->
-            <x-portfolio.about :entity="$entity->portfolio->data"/>
+            <div data-aos="fade-up" data-aos-anchor-placement="top-center">
+                <x-portfolio.about :entity="$entity->portfolio->data" />
+            </div>
             <!-- Work History -->
-            <x-portfolio.work-histories :entity="$entity->workHistories->data"/>
+            <div data-aos="fade-up" data-aos-anchor-placement="top-center">
+                <x-portfolio.work-histories :entity="$entity->workHistories->data" />
+            </div>
             <!-- Projects -->
-            <x-portfolio.projects :entity="$entity->projects->data"/>
+            <div data-aos="fade-up" data-aos-anchor-placement="top-center">
+                <x-portfolio.projects :entity="$entity->projects->data" />
+            </div>
             <!-- Socials -->
-            <x-portfolio.socials :entity="$entity->socials->data"/>
-        </div>
-        <!-- Footer -->
-        <x-portfolio.footer :entity="$entity->portfolio->data"/>
-    </div>
+            <div data-aos="fade-up" data-aos-anchor-placement="top-center">
+                <x-portfolio.socials :entity="$entity->socials->data" />
+            </div>
+            <!-- Footer -->
+            <x-portfolio.footer :entity="$entity->portfolio->data" />
 </x-app-layout>
