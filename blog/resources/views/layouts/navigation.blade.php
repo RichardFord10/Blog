4dd9f25cda3735af4ac7499f2f5811bbf46dac3f
Richard Ford <richardford10@gmail.com>
2024-04-28 00:28:01 -0400

    change blog

--- blog/resources/views/layouts/navigation.blade.php
+++ blog/resources/views/layouts/navigation.blade.php
@@ -253,7 +253,7 @@
         <div class="pt-2 pb-3 space-y-1">
 
             <x-responsive-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
-                {{ __('Blog Posts') }}
+                {{ __('Blog') }}
             </x-responsive-nav-link>
 
             <!-- <x-responsive-nav-link :href="route('chat.index')" :active="request()->routeIs('chat.index')">
