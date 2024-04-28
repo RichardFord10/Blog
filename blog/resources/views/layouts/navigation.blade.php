2a22eb682bcb2d19c6eb83191f1b1a34d45e8024
Richard Ford <richardford10@gmail.com>
2024-04-28 00:31:41 -0400

    remove hamburger

--- blog/resources/views/layouts/navigation.blade.php
+++ blog/resources/views/layouts/navigation.blade.php
@@ -195,13 +195,10 @@
                         Contact
                     </a>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <!-- Link -->
-                </div>
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="flex sm:items-center sm:ms-6">
 
                 <x-responsive-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
                     {{ __('Blog') }}
@@ -237,60 +234,13 @@
                 @endif -->
             </div>
 
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
+            <!-- Remove Hamburger Menu -->
+            <!-- Previously here was a div containing the hamburger menu button -->
+
         </div>
     </div>
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-
-            <x-responsive-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
-                {{ __('Blog') }}
-            </x-responsive-nav-link>
-
-            <!-- <x-responsive-nav-link :href="route('chat.index')" :active="request()->routeIs('chat.index')">
-                {{ __('ChatGPT Clone') }}
-            </x-responsive-nav-link>
-
-            <x-responsive-nav-link :href="route('imdb')" :active="request()->routeIs('imdb')">
-                {{ __('Data Table') }}
-            </x-responsive-nav-link>
-
-            <x-responsive-nav-link :href="route('finance-tracker')" :active="request()->routeIs('finance-tracker')">
-                {{ __('Finance Tracker') }}
-            </x-responsive-nav-link>
-
-            <x-responsive-nav-link :href="route('upload')" :active="request()->routeIs('upload')">
-                {{ __('CSV Tool') }}
-            </x-responsive-nav-link>
-
-            <x-responsive-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
-                {{ __('Product Filter') }}
-            </x-responsive-nav-link> -->
-
-            <!-- Responsive Settings Options -->
-            <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-
-                <div class="mt-3 space-y-1 pt-2 pb-3 space-y-1">
-                    <x-responsive-nav-link :href=" route('login')" :active="request()->routeIs('login')">
-                        {{ __('Login') }}
-                    </x-responsive-nav-link>
-                    <!-- @if (Route::has('register'))
-                    <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
-                        {{ __('Register') }}
-                    </x-responsive-nav-link>
-                    @endif -->
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- Removing responsive hidden menu to make sure navigation bar is simplified -->
 </nav>
+
 @endif
