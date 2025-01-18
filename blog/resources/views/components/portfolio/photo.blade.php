ba0ef1630da2c4a1db4a863c5afa0cda869a7e20
Richard Ford <richardford10@gmail.com>
2025-01-18 14:20:28 -0500

    oops

--- blog/resources/views/components/portfolio/photo.blade.php
+++ blog/resources/views/components/portfolio/photo.blade.php
@@ -3,7 +3,7 @@
         <div class="h-48 w-48 rounded-full overflow-hidden shadow-sm mx-auto">
             @isset($entity)
             <img
-                src="{{ asset('storage/' . $entity->image) }}"
+                src="{{ asset('storage/' . $entity) }}"
                 alt="Profile Image"
                 class="w-full h-full object-cover object-center">
             @endisset
